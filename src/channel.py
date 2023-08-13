7898ba822c62236e866eb72bfc5a783ba7867bcf
Veronika Surkova <surkova_nika92@mail.ru>
2023-08-13 18:08:39 +0300

    add functions to class Channel

--- src/channel.py
+++ src/channel.py
@@ -6,13 +6,36 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
     api_key = os.environ.get('YOUTUBE_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.__channel_id = channel_id
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = f'https://www.youtube.com/channel/{self.channel_id}'
+        self.subscriber_count = channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = channel["items"][0]["statistics"]["viewCount"]
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, value):
+        self.__channel_id = value
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    def to_json(self, file):
+        with open(file, "w", encoding="utf-8") as write_file:
+            json.dump(self.__dict__, write_file)
